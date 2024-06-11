d3f1798e6b92ead8cf5e5b883e21063cddf19909
Artuom4ik <masterskayaigr123@gmail.com>
2024-06-11 19:33:28 +0300

    Save messages history to file

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 import asyncio
+import datetime
+from contextlib import asynccontextmanager
+
+import aiofiles
+
 
 async def get_chat_messages():
-    reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
+    async with create_chat_connection('minechat.dvmn.org', 5000) as connection:
+        reader, writer = connection
 
-    while True:
-        message = await reader.readline()
-        print(message.decode())
+        while not reader.at_eof():
+            try:
+                message = await reader.readline()
+
+                decode_message = f"{datetime.datetime.now().strftime('[%d.%m.%y %H:%M]')} {message.decode('utf-8').strip()}\n"
+
+                async with aiofiles.open('messages.txt', 'a') as file:
+                    await file.write(decode_message)
+                
+                print(decode_message)
+            
+            except asyncio.CancelledError:
+                print("Ошибка сетевого подключения")
+
+
+@asynccontextmanager
+async def create_chat_connection(host, port):
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        yield reader, writer
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
 
 asyncio.run(get_chat_messages())

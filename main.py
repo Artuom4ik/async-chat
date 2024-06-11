cb6975618dd3c57b379b6dbe5f819bcbd7bee3a2
Artuom4ik <masterskayaigr123@gmail.com>
2024-06-11 19:49:01 +0300

    Add CLI options

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
+import os
+import argparse
 import asyncio
 import datetime
 from contextlib import asynccontextmanager
 
 import aiofiles
+from dotenv import load_dotenv
+
+
+def get_settings():
+    load_dotenv()
+    parser = argparse.ArgumentParser(
+        description='This is async chat. You can see history of messages by your link',
+    )
+
+    parser.add_argument(
+        "-ho",
+        "--host",
+        type=str,
+        default=os.getenv("HOST"),
+        help="Your host. For example: minechat.dvmn.org",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=os.getenv("PORT"),
+        help="Your port. For example: 5000"
+    )
+    parser.add_argument(
+        "-hp",
+        "--history_path",
+        type=str,
+        default=os.getenv("HISTORY_PATH"),
+        help="Path to history file. For example: messages.txt"
+    )
+
+    return parser.parse_args()
 
 
 async def get_chat_messages():
-    async with create_chat_connection('minechat.dvmn.org', 5000) as connection:
+    settings = get_settings()
+    async with create_chat_connection(settings.host, settings.port) as connection:
         reader, writer = connection
 
         while not reader.at_eof():
@@ -15,7 +50,7 @@ async def get_chat_messages():
 
                 decode_message = f"{datetime.datetime.now().strftime('[%d.%m.%y %H:%M]')} {message.decode('utf-8').strip()}\n"
 
-                async with aiofiles.open('messages.txt', 'a') as file:
+                async with aiofiles.open(settings.history_path, 'a') as file:
                     await file.write(decode_message)
                 
                 print(decode_message)

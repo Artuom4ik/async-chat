0ff6bc3f37a8a84e43e5c330e5ef3ba3c220d628
Artuom4ik <masterskayaigr123@gmail.com>
2024-08-17 20:38:03 +0300

    Move the code from gui to main

--- gui.py
+++ gui.py
@@ -1,31 +1,9 @@
-import os
-import json
-import logging
-import argparse
 import asyncio
 import tkinter as tk
-from tkinter import messagebox
 from enum import Enum
 from tkinter.scrolledtext import ScrolledText
-from contextlib import asynccontextmanager
 
-import anyio
-import anyio.abc
 from anyio import create_task_group
-import aiofiles
-import async_timeout
-from dotenv import load_dotenv
-
-
-class UnixTimeFormatter(logging.Formatter):
-    def formatTime(self, record, datefmt=None):
-        return f"[{int(record.created)}]"
-
-
-class Invalidtoken(Exception):
-    def __init__(self, message="Invalid token", errors=None):
-        super().__init__(message)
-        self.errors = errors
 
 
 class TkAppClosed(Exception):
@@ -55,71 +33,6 @@ class NicknameReceived:
         self.nickname = nickname
 
 
-def get_settings():
-    load_dotenv()
-    parser = argparse.ArgumentParser(
-        description='This is async chat. You can see history of messages by your link',
-    )
-
-    parser.add_argument(
-        "-gh",
-        "--get_host",
-        type=str,
-        default=os.getenv("GET_HOST"),
-        help="Your host. For example: minechat.dvmn.org",
-    )
-
-    parser.add_argument(
-        "-ph",
-        "--post_host",
-        type=str,
-        default=os.getenv("POST_HOST"),
-        help="Your host. For example: minechat.dvmn.org",
-    )
-
-    parser.add_argument(
-        "-gp",
-        "--get_port",
-        type=int,
-        default=os.getenv("GET_PORT"),
-        help="Your port. For example: 5000"
-    )
-
-    parser.add_argument(
-        "-pp",
-        "--post_port",
-        type=int,
-        default=os.getenv("POST_PORT"),
-        help="Your port. For example: 5050"
-    )
-
-    parser.add_argument(
-        "-t",
-        "--token",
-        type=str,
-        default="",
-        help="Your authorization token.",
-    )
-
-    parser.add_argument(
-        "-n",
-        "--name",
-        type=str,
-        default="",
-        help="If you are not authorized, you can enter a login to register",
-    )
-
-    parser.add_argument(
-        "-hp",
-        "--history_path",
-        type=str,
-        default=os.getenv("HISTORY_PATH"),
-        help="Path to history file. For example: messages.txt"
-    )
-
-    return parser.parse_args()
-
-
 def process_new_message(input_field, sending_queue):
     text = input_field.get()
     sending_queue.put_nowait(text)
@@ -133,26 +46,22 @@ async def update_tk(root_frame, interval=1 / 120):
         except tk.TclError:
             # if application has been destroyed/closed
             raise TkAppClosed()
-        await anyio.sleep(interval)
+        await asyncio.sleep(interval)
 
 
 async def update_conversation_history(panel, messages_queue):
     while True:
-        try:
-            msg = await messages_queue.get()
+        msg = await messages_queue.get()
 
-            panel['state'] = 'normal'
-            if panel.index('end-1c') != '1.0':
-                panel.insert('end', '\n')
-            panel.insert('end', msg)
-            # TODO сделать промотку умной, чтобы не мешала просматривать историю сообщений
-            # ScrolledText.frame
-            # ScrolledText.vbar
-            panel.yview(tk.END)
-            panel['state'] = 'disabled'
-
-        except asyncio.CancelledError:
-            break
+        panel['state'] = 'normal'
+        if panel.index('end-1c') != '1.0':
+            panel.insert('end', '\n')
+        panel.insert('end', msg)
+        # TODO сделать промотку умной, чтобы не мешала просматривать историю сообщений
+        # ScrolledText.frame
+        # ScrolledText.vbar
+        panel.yview(tk.END)
+        panel['state'] = 'disabled'
 
 
 async def update_status_panel(status_labels, status_updates_queue):
@@ -163,19 +72,15 @@ async def update_status_panel(status_labels, status_updates_queue):
     nickname_label['text'] = f'Имя пользователя: неизвестно'
 
     while True:
-        try:
-            msg = await status_updates_queue.get()
-            if isinstance(msg, ReadConnectionStateChanged):
-                read_label['text'] = f'Чтение: {msg}'
+        msg = await status_updates_queue.get()
+        if isinstance(msg, ReadConnectionStateChanged):
+            read_label['text'] = f'Чтение: {msg}'
 
-            if isinstance(msg, SendingConnectionStateChanged):
-                write_label['text'] = f'Отправка: {msg}'
+        if isinstance(msg, SendingConnectionStateChanged):
+            write_label['text'] = f'Отправка: {msg}'
 
-            if isinstance(msg, NicknameReceived):
-                nickname_label['text'] = f'Имя пользователя: {msg.nickname}'
-
-        except asyncio.CancelledError:
-            break
+        if isinstance(msg, NicknameReceived):
+            nickname_label['text'] = f'Имя пользователя: {msg.nickname}'
 
 
 def create_status_panel(root_frame):
@@ -186,29 +91,15 @@ def create_status_panel(root_frame):
     connections_frame.pack(side="left")
 
     nickname_label = tk.Label(
-        connections_frame,
-        height=1,
-        fg='grey',
-        font='arial 10',
-        anchor='w'
-    )
+        connections_frame, height=1, fg='grey', font='arial 10', anchor='w')
     nickname_label.pack(side="top", fill=tk.X)
 
     status_read_label = tk.Label(
-        connections_frame,
-        height=1, fg='grey',
-        font='arial 10',
-        anchor='w'
-    )
+        connections_frame, height=1, fg='grey', font='arial 10', anchor='w')
     status_read_label.pack(side="top", fill=tk.X)
 
     status_write_label = tk.Label(
-        connections_frame,
-        height=1,
-        fg='grey',
-        font='arial 10',
-        anchor='w'
-    )
+        connections_frame, height=1, fg='grey', font='arial 10', anchor='w')
     status_write_label.pack(side="top", fill=tk.X)
 
     return (nickname_label, status_read_label, status_write_label)
@@ -232,7 +123,8 @@ async def draw(messages_queue, sending_queue, status_updates_queue):
 
     input_field.bind("<Return>", lambda event: process_new_message(
         input_field,
-        sending_queue))
+        sending_queue
+    ))
 
     send_button = tk.Button(input_frame)
     send_button["text"] = "Отправить"
@@ -247,305 +139,7 @@ async def draw(messages_queue, sending_queue, status_updates_queue):
 
     async with create_task_group() as task_group:
         task_group.start_soon(update_tk, root_frame)
-
         task_group.start_soon(
-            update_conversation_history,
-            conversation_panel,
-            messages_queue
-        )
-
+            update_conversation_history, conversation_panel, messages_queue)
         task_group.start_soon(
-            update_status_panel,
-            status_labels,
-            status_updates_queue
-        )
-
-
-def escape_stickiness_removed(text):
-    return text.replace("\\n", " ").strip()
-
-
-async def authorise(
-        account_hash,
-        post_reader,
-        post_writer,
-        status_updates_queue,
-        watchdog_queue):
-
-    watchdog_queue.put_nowait('Prompt before auth')
-
-    data = await post_reader.read(200)
-
-    post_writer.write((account_hash + "\n").encode())
-
-    data = await post_reader.read(200)
-
-    if json.loads(data.decode().split("\n")[0]) is None:
-        raise Invalidtoken
-
-    nickname = json.loads(data.decode().split("\n")[0])['nickname']
-
-    status_updates_queue.put_nowait(NicknameReceived(nickname=nickname))
-
-    status_updates_queue.put_nowait(SendingConnectionStateChanged.ESTABLISHED)
-
-    watchdog_queue.put_nowait('Authorization done')
-
-
-async def save_messages(filepath, queue):
-    message = await queue.get()
-
-    decode_message = f"{message}\n"
-
-    async with aiofiles.open(filepath, 'a') as file:
-        await file.write(decode_message)
-
-
-async def read_msgs(
-        get_host,
-        get_port,
-        messages_queue,
-        history_message_queue,
-        watchdog_queue,
-        settings,
-        status_updates_queue):
-
-    while True:
-        status_updates_queue.put_nowait(
-            ReadConnectionStateChanged.INITIATED
-        )
-
-        async with create_chat_connection(get_host, get_port) as connection:
-            reader, writer = connection
-
-            status_updates_queue.put_nowait(ReadConnectionStateChanged.ESTABLISHED)
-
-            try:
-                while not reader.at_eof():
-                    async with async_timeout.timeout(10):
-                        message = await reader.readline()
-
-                        message = message.decode('utf-8').strip()
-
-                        messages_queue.put_nowait(message)
-                        history_message_queue.put_nowait(message)
-
-                        watchdog_queue.put_nowait('New message in chat')
-
-                        await save_messages(
-                            settings.history_path,
-                            history_message_queue
-                        )
-
-            except asyncio.TimeoutError:
-                status_updates_queue.put_nowait(
-                    ReadConnectionStateChanged.CLOSED
-                )
-                watchdog_queue.put_nowait('1s timeout is elapsed')
-
-
-async def send_msgs(
-        post_host,
-        post_port,
-        sending_queue,
-        watchdog_queue,
-        settings,
-        auth_file_name,
-        status_updates_queue):
-
-    status_updates_queue.put_nowait(
-        SendingConnectionStateChanged.INITIATED
-    )
-
-    async with create_chat_connection(post_host, post_port) as (post_reader, post_writer):
-        if settings.token:
-            await authorise(
-                settings.token,
-                post_reader,
-                post_writer,
-                status_updates_queue,
-                watchdog_queue
-            )
-
-        elif os.path.exists(auth_file_name):
-            async with aiofiles.open(auth_file_name, 'r') as file:
-                account_data = await file.read()
-
-            account_data = json.loads(account_data)
-
-            await authorise(
-                account_data["account_hash"],
-                post_reader,
-                post_writer,
-                status_updates_queue,
-                watchdog_queue
-            )
-
-        while True:
-            message = await sending_queue.get()
-
-            message = escape_stickiness_removed(message)
-
-            post_writer.write((message + "\n\n").encode())
-
-            watchdog_queue.put_nowait('Message sent')
-
-
-@asynccontextmanager
-async def create_chat_connection(host, port):
-    reader, writer = await asyncio.open_connection(host, port)
-    try:
-        yield reader, writer
-    finally:
-        writer.close()
-        await writer.wait_closed()
-
-
-async def watch_for_connection(watchdog_queue, watchdog_logger):
-    while True:
-        try:
-            msg = await watchdog_queue.get()
-            watchdog_logger.info('Connection is alive. %s', msg)
-
-        except asyncio.CancelledError:
-            watchdog_logger.warning('Connection aborted')
-            raise
-
-
-async def handle_connection(
-        get_host,
-        get_port,
-        post_host,
-        post_port,
-        status_updates_queue,
-        messages_queue,
-        sending_queue,
-        watchdog_queue,
-        history_message_queue,
-        watchdog_logger,
-        settings,
-        auth_file_name):
-
-    # while True:
-    try:
-        async with create_task_group() as task_group:
-            status_updates_queue.put_nowait(
-                ReadConnectionStateChanged.INITIATED
-            )
-            status_updates_queue.put_nowait(
-                SendingConnectionStateChanged.INITIATED
-            )
-
-            task_group.start_soon(
-                read_msgs,
-                get_host,
-                get_port,
-                messages_queue,
-                history_message_queue,
-                watchdog_queue,
-                settings,
-                status_updates_queue
-            )
-            task_group.start_soon(
-                send_msgs,
-                post_host,
-                post_port,
-                sending_queue,
-                watchdog_queue,
-                settings,
-                auth_file_name,
-                status_updates_queue
-            )
-
-            task_group.start_soon(
-                watch_for_connection,
-                watchdog_queue,
-                watchdog_logger)
-
-    except BaseException as e:
-        if isinstance(e, ConnectionError):
-            status_updates_queue.put_nowait(
-                ReadConnectionStateChanged.CLOSED
-            )
-            status_updates_queue.put_nowait(
-                SendingConnectionStateChanged.CLOSED
-            )
-
-        # continue
-
-
-def load_from_file(history_path, messages_queue):
-    if os.path.exists(history_path):
-        with open(history_path, 'r') as file:
-            for line in file.readlines():
-                messages_queue.put_nowait(line.replace('\n', ''))
-
-
-async def main():
-    formatter = UnixTimeFormatter(
-        '%(asctime)s %(name)s %(levelname)s: %(message)s'
-    )
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-
-    logging.basicConfig(level=logging.DEBUG, handlers=[handler,])
-    watchdog_logger = logging.getLogger('watchdog')
-
-    settings = get_settings()
-
-    history_message_queue = asyncio.Queue()
-    messages_queue = asyncio.Queue()
-    sending_queue = asyncio.Queue()
-    status_updates_queue = asyncio.Queue()
-    watchdog_queue = asyncio.Queue()
-
-    auth_file_name = "auth.json"
-
-    get_host = settings.get_host
-    post_host = settings.post_host
-    get_port = settings.get_port
-    post_port = settings.post_port
-
-    load_from_file(settings.history_path, messages_queue)
-
-    try:
-        async with create_task_group() as task_group:
-            task_group.start_soon(
-                draw,
-                messages_queue,
-                sending_queue,
-                status_updates_queue
-            )
-            task_group.start_soon(
-                handle_connection,
-                get_host,
-                get_port,
-                post_host,
-                post_port,
-                status_updates_queue,
-                messages_queue,
-                sending_queue,
-                watchdog_queue,
-                history_message_queue,
-                watchdog_logger,
-                settings,
-                auth_file_name
-            )
-
-    except Invalidtoken:
-        messagebox.showinfo(
-            'Неверный токен',
-            'Проверьте токен, сервер его не узнал'
-        )
-
-    except (KeyboardInterrupt, TkAppClosed):
-        logging.info("Application closed")
-
-
-if __name__ == '__main__':
-    try:
-        anyio.run(main)
-    except KeyboardInterrupt:
-        logging.info("Program interrupted by user")
-    finally:
-        logging.info("Program exited gracefully")
+            update_status_panel, status_labels, status_updates_queue)
---
+++ main.py
@@ -0,0 +1,350 @@
+import os
+import json
+import logging
+import asyncio
+import argparse
+from tkinter import messagebox
+from contextlib import asynccontextmanager
+
+import aiofiles
+from dotenv import load_dotenv
+from async_timeout import timeout
+from anyio import create_task_group, run
+
+import gui
+
+
+class UnixTimeFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        return f"[{int(record.created)}]"
+
+
+class Invalidtoken(Exception):
+    def __init__(self, message="Invalid token", errors=None):
+        super().__init__(message)
+        self.errors = errors
+
+
+def get_settings():
+    load_dotenv()
+    parser = argparse.ArgumentParser(
+        description='This is async chat. You can send messages',
+    )
+
+    parser.add_argument(
+        "-t",
+        "--token",
+        type=str,
+        default="",
+        help="Your token. For example: bcd77b02-4e86-11ef-abed-02"
+    )
+
+    parser.add_argument(
+        "-ph",
+        "--post_host",
+        type=str,
+        default=os.getenv("POST_HOST"),
+        help="Your host. For example: minechat.dvmn.org",
+    )
+
+    parser.add_argument(
+        "-pp",
+        "--post_port",
+        type=int,
+        default=os.getenv("POST_PORT"),
+        help="Your port. For example: 5050"
+    )
+
+    parser.add_argument(
+        "-gh",
+        "--get_host",
+        type=str,
+        default=os.getenv("GET_HOST"),
+        help="Your host. For example: minechat.dvmn.org",
+    )
+
+    parser.add_argument(
+        "-gp",
+        "--get_port",
+        type=int,
+        default=os.getenv("GET_PORT"),
+        help="Your port. For example: 5000"
+    )
+
+    parser.add_argument(
+        "-hp",
+        "--history_path",
+        type=str,
+        default=os.getenv("HISTORY_PATH"),
+        help="Path to history file. For example: messages.txt"
+    )
+
+    return parser.parse_args()
+
+
+def escape_stickiness_removed(text):
+    return text.replace("\\n", " ").strip()
+
+
+async def watch_for_connection(watchdog_queue, watchdog_logger):
+    while True:
+        try:
+            async with timeout(10):
+                msg = await watchdog_queue.get()
+                watchdog_logger.info('Connection is alive. %s', msg)
+
+        except asyncio.TimeoutError:
+            watchdog_logger.warning('Reconnecting...')
+            raise ConnectionError
+
+
+@asynccontextmanager
+async def create_chat_connection(host, port):
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        yield reader, writer
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
+async def save_messages(history_message_queue, history_path):
+    async with aiofiles.open(history_path, 'a') as file:
+        while True:
+            msg = await history_message_queue.get()
+            await file.write(msg)
+
+
+async def read_messages(
+        messages_queue,
+        history_message_queue,
+        get_host,
+        get_port,
+        status_updates_queue,
+        watchdog_queue):
+
+    status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.INITIATED)
+
+    async with create_chat_connection(get_host, get_port) as (reader, writer):
+        status_updates_queue.put_nowait(
+            gui.ReadConnectionStateChanged.ESTABLISHED)
+
+        while True:
+            message = await reader.readline()
+
+            messages_queue.put_nowait(
+                escape_stickiness_removed(message.decode()))
+
+            history_message_queue.put_nowait(message.decode())
+
+            watchdog_queue.put_nowait("New chat message")
+
+
+async def authorise(account_hash, post_reader, post_writer):
+    data = await post_reader.read(200)
+
+    post_writer.write((account_hash + "\n").encode())
+
+    data = await post_reader.read(200)
+
+    if json.loads(data.decode().split("\n")[0]) is None:
+        raise Invalidtoken
+
+    nickname = json.loads(data.decode().split("\n")[0])['nickname']
+
+    return nickname
+
+
+async def send_messages(
+        post_host,
+        post_port,
+        auth_file_path,
+        sending_queue,
+        token,
+        status_updates_queue,
+        watchdog_queue):
+
+    status_updates_queue.put_nowait(
+        gui.SendingConnectionStateChanged.INITIATED)
+
+    async with create_chat_connection(post_host, post_port) as (post_reader, post_writer):
+        status_updates_queue.put_nowait(
+            gui.SendingConnectionStateChanged.ESTABLISHED)
+
+        watchdog_queue.put_nowait("Prompt before auth")
+
+        if token:
+            nickname = await authorise(token, post_reader, post_writer)
+
+        elif os.path.exists(auth_file_path):
+            async with aiofiles.open(auth_file_path, 'r') as file:
+                account_data = await file.read()
+                account_hash = json.loads(account_data)['account_hash']
+
+                nickname = await authorise(
+                    account_hash,
+                    post_reader,
+                    post_writer
+                )
+
+        else:
+            raise Invalidtoken
+
+        status_updates_queue.put_nowait(gui.NicknameReceived(nickname))
+        watchdog_queue.put_nowait("Authorization done")
+
+        while True:
+            message = await sending_queue.get()
+
+            message = escape_stickiness_removed(message)
+
+            post_writer.write((message + "\n\n").encode())
+
+            watchdog_queue.put_nowait("Message sent")
+
+
+async def handle_connection(
+        token,
+        get_host,
+        get_port,
+        post_host,
+        post_port,
+        history_path,
+        auth_file_path,
+        watchdog_logger,
+        sending_queue,
+        messages_queue,
+        watchdog_queue,
+        status_updates_queue,
+        history_message_queue):
+
+    while True:
+        try:
+            async with create_task_group() as task_group:
+                task_group.start_soon(
+                    read_messages,
+                    messages_queue,
+                    history_message_queue,
+                    get_host,
+                    get_port,
+                    status_updates_queue,
+                    watchdog_queue
+                )
+
+                task_group.start_soon(
+                    send_messages,
+                    post_host,
+                    post_port,
+                    auth_file_path,
+                    sending_queue,
+                    token,
+                    status_updates_queue,
+                    watchdog_queue
+                )
+
+                task_group.start_soon(
+                    watch_for_connection,
+                    watchdog_queue,
+                    watchdog_logger
+                )
+
+                task_group.start_soon(
+                    save_messages,
+                    history_message_queue,
+                    history_path
+                )
+
+        except BaseException as e:
+            if isinstance(e, ConnectionError):
+                status_updates_queue.put_nowait(
+                    gui.ReadConnectionStateChanged.CLOSED)
+
+                status_updates_queue.put_nowait(
+                    gui.SendingConnectionStateChanged.CLOSED)
+
+                continue
+
+            if isinstance(e, asyncio.CancelledError):
+                break
+
+
+async def main():
+    formatter = UnixTimeFormatter(
+        '%(asctime)s %(name)s %(levelname)s: %(message)s'
+    )
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logging.basicConfig(level=logging.DEBUG, handlers=[handler,])
+    watchdog_logger = logging.getLogger('watchdog')
+
+    settings = get_settings()
+
+    get_host = settings.get_host
+    get_port = settings.get_port
+
+    post_host = settings.post_host
+    post_port = settings.post_port
+
+    token = settings.token
+
+    history_path = settings.history_path
+
+    auth_file_path = "auth.json"
+
+    history_message_queue = asyncio.Queue()
+    messages_queue = asyncio.Queue()
+    sending_queue = asyncio.Queue()
+    status_updates_queue = asyncio.Queue()
+    watchdog_queue = asyncio.Queue()
+
+    if os.path.exists(history_path):
+        with open(history_path, 'r') as file:
+            while True:
+                msg = file.readline()
+                if msg:
+                    messages_queue.put_nowait(escape_stickiness_removed(msg))
+                else:
+                    break
+
+    try:
+        async with create_task_group() as task_group:
+            task_group.start_soon(
+                gui.draw,
+                messages_queue,
+                sending_queue,
+                status_updates_queue
+            )
+
+            task_group.start_soon(
+                handle_connection,
+                token,
+                get_host,
+                get_port,
+                post_host,
+                post_port,
+                history_path,
+                auth_file_path,
+                watchdog_logger,
+                sending_queue,
+                messages_queue,
+                watchdog_queue,
+                status_updates_queue,
+                history_message_queue
+            )
+
+    except Invalidtoken:
+        messagebox.showerror(
+           "Неверный токен",
+           "Проверьте токен, сервер его не узнал."
+        )
+        exit(0)
+
+
+if __name__ == '__main__':
+    try:
+        run(main)
+    except asyncio.CancelledError:
+        logging.info("Program interrupted by user")
+    except BaseException:
+        logging.info("Program exited gracefully")
